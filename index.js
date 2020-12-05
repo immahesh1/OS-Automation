c29525e333e2eb01a2e4b835b461d1497c203864
m1github <mahesh_kumar1019@yahoo.in>
2020-12-05 18:09:32 +0530

    Completed a Mini Project

--- index.js
+++ index.js
@@ -1,3 +1,63 @@
 var robot = require('robotjs')
-var mouse = robot.getMousePos();
-console.log(mouse.x);
+var fs = require('fs')
+
+setTimeout(openGITBash,2000);
+
+
+function openGITBash(){
+    robot.moveMouseSmooth(53,750)
+    robot.mouseClick()
+    robot.typeString("GIT Bash")
+    robot.keyTap("enter")
+    setTimeout(openChrome,2000);
+}
+function openChrome(){
+    robot.moveMouseSmooth(53,750)
+    robot.mouseClick()
+    robot.typeString("google chrome")
+    robot.keyTap("enter")
+    // Opening tabs
+    setTimeout(openTabs,2000);
+}
+
+function openTabs(){
+    var rawdata = fs.readFileSync("./chrome.json")
+    var tabs = JSON.parse(rawdata)
+    for(var i=0; i<tabs.length; i++){
+        for(var j=0; j<tabs[i].length; j++){
+            robot.typeString(tabs[i][j])
+            robot.keyTap("enter")
+
+            if(j < tabs[i].length-1){
+                robot.keyToggle("control","down")
+                robot.keyTap("T")
+                robot.keyToggle("control","up")    
+            }else if(i < tabs.length - 1){
+                robot.keyToggle("control","down")
+                robot.keyTap("n")
+                robot.keyToggle("control","up") 
+            }
+        }
+    }
+    setTimeout(openVsCode,7000);
+}
+
+function openVsCode(){
+    robot.moveMouseSmooth(53,750)
+    robot.mouseClick()
+    robot.typeString("Visual Studio Code")
+    robot.keyTap("enter")
+    setTimeout(openNotePad,12000);
+}
+
+function openNotePad(){
+    robot.moveMouseSmooth(53,750)
+    robot.mouseClick()
+    robot.typeString("Notepad")
+    robot.keyTap("enter")
+    setTimeout(function(){
+        robot.typeStringDelayed("Setup is Ready! ")
+        robot.typeStringDelayed("Please start working...")
+    },2000)
+}
+
